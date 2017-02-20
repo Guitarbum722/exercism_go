2c37f90f3f782d2da25e1f09e38021ab3925a4bc
guitarbum722 <johnkenneth.moore@gmail.com>
2017-02-19 17:48:51 -0700

    Remove extraneous comments

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -9,9 +9,6 @@ const testVersion = 5
 
 // Distance accepts two strings representing strands of DNA, and returns the hamming distance of 0 or more.  Note that 0 will also be returned if err is not nil.
 func Distance(a, b string) (int, error) {
-	// if a == "" || b == "" {
-	// 	return 0, errors.New("Two input strings must be provided")
-	// }
 	if len(a) != len(b) {
 		return -1, errors.New("Inputs must be of equal length")
 	}

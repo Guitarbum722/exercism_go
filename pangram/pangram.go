d0577f7612909a72a1299069bdf081f72f2038fe
guitarbum722 <johnkenneth.moore@gmail.com>
2017-03-05 20:44:06 -0700

    Add pangram exercise.

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -8,26 +8,13 @@ import (
 const testVersion = 1
 
 // IsPangram returns true if input contains all of the letters in alphabet.  The input must contain only ASCII characters.
-func IsPangram(input string) bool {
-	var letterList = make(map[rune]int)
-
-	if input == "" {
-		return false
-	}
-	for r := 'a'; r <= 'z'; r++ {
-		letterList[r] = 0
-	}
-	input = strings.ToLower(input)
-	for _, v := range input {
-		if v < 'a' || v > 'z' {
-			continue
-		}
-		letterList[v] = 1
-	}
-	for k := range letterList {
-		if letterList[k] < 1 {
-			return false
+func IsPangram(s string) bool {
+	lowerLetters := make(map[rune]uint8)
+	s = strings.ToLower(s)
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' {
+			lowerLetters[r] = 1
 		}
 	}
-	return true
+	return len(lowerLetters) == 26
 }
